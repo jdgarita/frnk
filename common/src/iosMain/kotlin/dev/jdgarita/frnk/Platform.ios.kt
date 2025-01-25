39c72e8656cb97af72a0d435f83ec4324edb9656
jd <juandiegogarita@gmail.com>
2025-01-25 09:40:18 -0600

    updates greeting class to mention frnk framework

--- common/src/iosMain/kotlin/dev/jdgarita/frnk/Platform.ios.kt
+++ common/src/iosMain/kotlin/dev/jdgarita/frnk/Platform.ios.kt
@@ -3,7 +3,7 @@ package dev.jdgarita.frnk
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion + " from Frnk framework"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
