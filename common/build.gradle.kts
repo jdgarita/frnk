62eb61af8cfebb0b0309557a156c81a3f23d9f78
JD <juandiegogarita@gmail.com>
2025-01-29 21:25:42 -0600

    updates versions

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -43,9 +43,9 @@ kotlin {
 
 android {
     namespace = "dev.jdgarita.frnk"
-    compileSdk = 35
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 
     compileOptions {
