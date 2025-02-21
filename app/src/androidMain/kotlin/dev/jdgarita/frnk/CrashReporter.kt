53f79d4a49932eaa6513ff0be749112058bc5b68
jd <juandiegogarita@gmail.com>
2025-02-20 21:58:20 -0600

    expect/actual crash reporter

--- app/src/androidMain/kotlin/dev/jdgarita/frnk/CrashReporter.kt
+++ app/src/androidMain/kotlin/dev/jdgarita/frnk/CrashReporter.kt
@@ -3,17 +3,18 @@ package dev.jdgarita.frnk
 import io.sentry.kotlin.multiplatform.Sentry
 import io.sentry.kotlin.multiplatform.SentryOptions
 
-class CrashReporter {
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class CrashReporter actual constructor() {
 
     // Application context is only needed for Android targets
-    fun initializeSentry(key: String) {
+    actual fun init(key: String) {
         val configuration: (SentryOptions) -> Unit = {
             it.dsn = key
         }
         Sentry.init(configuration)
     }
 
-    fun captureError() {
+    actual fun captureError() {
         try {
             throw Exception("This is a test.")
         } catch (e: Exception) {
---
+++ app/src/iosMain/kotlin/dev/jdgarita/frnk/CrashReporter.ios.kt
@@ -0,0 +1,10 @@
+package dev.jdgarita.frnk
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class CrashReporter actual constructor() {
+    actual fun init(key: String) {
+    }
+
+    actual fun captureError() {
+    }
+}
