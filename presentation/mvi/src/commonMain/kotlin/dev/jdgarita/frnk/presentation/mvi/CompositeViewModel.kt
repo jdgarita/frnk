54dc44259e754d591f21f8af27b0db0e2b140d74
jd <juandiegogarita@gmail.com>
2025-03-02 19:29:25 -0600

    feat: introduces CompositeViewModel for managing child ViewModels
    This commit introduces the `CompositeViewModel` class, designed to manage a collection of child `ViewModels`.
    
    Key features and changes:
    
    -   **CompositeViewModel:** A new class, `CompositeViewModel`, is introduced. It extends `BaseFrnkMviModel` and implements `MviViewModel`, `LifecycleAware`, and `DynamicViewModelParent`. It's designed to manage multiple child `ViewModels`, coordinating their states and lifecycle.
    -   **Dynamic Child ViewModel Management:** The `CompositeViewModel` supports dynamic addition and removal of child `ViewModels` through `addDynamicChildViewModel` and `clearDynamicChildViewModels`.
    -   **State Aggregation:** It observes and aggregates common view states (like loading, error, toast alerts, dialogs, bottom sheets) from child `ViewModels`.
    -   **Child Lifecycle Management:** It handles the lifecycle events of child `ViewModels` (`attach`, `activate`, `deactivate`, `detach`).
    -   **External Event Handling:** It facilitates the handling of external events from child `ViewModels`.
    -   **Refreshable Children:** It supports selectively refreshing children ViewModels.
    -   **Child Arguments:** Uses `getChildArguments` to retrieve arguments for each child ViewModel.
    -   **Child external events:** It also handles child external events and propagates them to the main external event flow.
    -   **Child ViewModels state observation:** Observes child common view state and combines them to propagate a common view state up the chain.
    - **Child ViewModel management**: Supports addition and removal of child ViewModels.
    - **CommonIntent handling**: Handles `CommonIntent.OnRefresh` by propagating it to children.
    - **Implementation**: It includes a `handleChildExternalEvent` method to be implemented by derived classes.

---
+++ presentation/mvi/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/mvi/CompositeViewModel.kt
@@ -0,0 +1,268 @@
+package dev.jdgarita.frnk.presentation.mvi
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.combine
+
+interface DynamicViewModelParent {
+
+    // One dimensional list (for now) of child view models
+    val dynamicChildViewModels: List<MviViewModel<out Arguments, *, *, *>>
+
+    /**
+     * Whether dynamic child view models should be refreshed when parent is refreshed.
+     * Usually it's not necessary because dynamic child view models are recreated when parent is refreshed
+     * so it might result in duplicated data loading for dynamic child view models.
+     */
+    val dynamicChildViewModelsRefreshable: Boolean
+    fun <TArguments : Arguments> addDynamicChildViewModel(child: MviViewModel<TArguments, *, *, *>, args: TArguments)
+    fun clearDynamicChildViewModels()
+}
+
+abstract class CompositeViewModel<
+    TModelState : CompositeStateCommon<TModelState>,
+    TArgs : Arguments,
+    TIntent : Intent,
+    TViewState : ViewStateCommon<TViewState>,
+    TExternalEvent : ExternalEvent
+    >(
+    childViewModels: List<MviViewModel<out Arguments, *, *, *>>,
+    singleThreadDispatcher: CoroutineDispatcher,
+    private val viewModelDependencies: ViewModelDependencies,
+    modelStateFactory: ModelStateFactory<TModelState>
+) : BaseFrnkMviModel<TModelState, TArgs, TIntent, TViewState, TExternalEvent>(
+    singleThreadDispatcher = singleThreadDispatcher,
+    modelStateFactory = modelStateFactory,
+    viewModelDependencies = viewModelDependencies
+),
+    MviViewModel<TArgs, TIntent, TViewState, TExternalEvent>,
+    LifecycleAware<TArgs>,
+    DynamicViewModelParent {
+
+    abstract fun handleChildExternalEvent(externalEvent: ExternalEvent)
+
+    override val dynamicChildViewModelsRefreshable: Boolean = false
+
+    private val _initialChildViewModels: List<MviViewModel<out Arguments, *, *, *>> = childViewModels
+    private val _dynamicChildViewModels: MutableList<Pair<MviViewModel<out Arguments, *, *, *>, Arguments>> =
+        mutableListOf()
+    override val dynamicChildViewModels: List<MviViewModel<out Arguments, *, *, *>>
+        get() = _dynamicChildViewModels.map { it.first }
+
+    private val childViewModels: List<MviViewModel<out Arguments, *, *, *>>
+        get() = _initialChildViewModels + _dynamicChildViewModels.map { it.first }
+
+    private var childrenCommonViewStateJob: Job? = null
+
+    override fun <TArguments : Arguments> addDynamicChildViewModel(
+        child: MviViewModel<TArguments, *, *, *>,
+        args: TArguments
+    ) {
+        _dynamicChildViewModels.add(Pair(child, args))
+        child.attachView(args)
+
+        child.cancelCallbackSubscriptions()
+        child.subscribeExternalEvents {
+            if (it is CommonExternalEvent) {
+                handleExternalEvent(it)
+            } else {
+                handleChildExternalEvent(it)
+            }
+        }
+
+        observeChildrenCommonViewState()
+        if (active) {
+            child.activate()
+        }
+    }
+
+    override fun clearDynamicChildViewModels() {
+        _dynamicChildViewModels.forEach {
+            it.first.deactivate()
+            it.first.detachView()
+        }
+        _dynamicChildViewModels.clear()
+    }
+
+    override fun onAttached(args: TArgs) {
+        super.onAttached(args)
+
+        observeChildrenCommonViewState()
+        attachChildren(args)
+    }
+
+    private fun observeChildrenCommonViewState() {
+        // Take a snapshot of the current child view models before launching the coroutine
+        // otherwise we risk concurrent modification exceptions
+        val childViewModelsSnapshot = childViewModels
+        childrenCommonViewStateJob?.cancel()
+        childrenCommonViewStateJob = launchAttached {
+            val childCommonViewStateFlows = childViewModelsSnapshot
+                .filter { it.viewState is ViewStateCommon<*> }
+                .map { it.viewStateFlow }
+
+            combine(childCommonViewStateFlows) { viewStates ->
+                val commonStates = viewStates.map { (it as ViewStateCommon<*>).commonViewState }
+                val loadingStates = commonStates.filter { it.dataLoadState is LoadState.Loading }
+                val isRefreshing = loadingStates.any { (it.dataLoadState as LoadState.Loading).isRefreshing }
+                // Assumption: there is only one toast alert view state in the children view states
+                val toastAlertViewState = commonStates
+                    .firstOrNull { it.toastAlertViewState != null }
+                    ?.toastAlertViewState
+                // Assumption: there is only one dialog in the children view states
+                val dialogViewState = commonStates
+                    .firstOrNull { it.dialogViewState != null }
+                    ?.dialogViewState
+                // Assumption: there is only one bottom sheet in the children view states
+                val bottomSheetViewState = commonStates
+                    .firstOrNull { it.bottomSheetViewState != null }
+                    ?.bottomSheetViewState
+                val appRatingVisible = commonStates.any { it.appRatingVisible }
+
+                when {
+                    loadingStates.isNotEmpty() -> CommonViewState(
+                        dataLoadState = LoadState.Loading(isRefreshing),
+                        toastAlertViewState = toastAlertViewState,
+                        dialogViewState = dialogViewState,
+                        bottomSheetViewState = bottomSheetViewState,
+                        appRatingVisible = appRatingVisible
+                    )
+                    commonStates.any { it.dataLoadState == LoadState.Failed } -> {
+                        val stateWithDisplayError = commonStates.firstOrNull { it.commonDisplayError != null }
+                        CommonViewState(
+                            dataLoadState = LoadState.Failed,
+                            commonDisplayError = stateWithDisplayError?.commonDisplayError,
+                            toastAlertViewState = toastAlertViewState,
+                            dialogViewState = dialogViewState,
+                            bottomSheetViewState = bottomSheetViewState,
+                            appRatingVisible = appRatingVisible
+                        )
+                    }
+                    commonStates.all { it.dataLoadState == LoadState.Loaded } -> {
+                        CommonViewState(
+                            dataLoadState = LoadState.Loaded,
+                            toastAlertViewState = toastAlertViewState,
+                            dialogViewState = dialogViewState,
+                            bottomSheetViewState = bottomSheetViewState,
+                            appRatingVisible = appRatingVisible
+                        )
+                    }
+                    else -> CommonViewState(
+                        dataLoadState = LoadState.Initialized,
+                        toastAlertViewState = toastAlertViewState,
+                        dialogViewState = dialogViewState,
+                        bottomSheetViewState = bottomSheetViewState,
+                        appRatingVisible = appRatingVisible
+                    )
+                }
+            }.collect { commonViewState ->
+                handleAggregatedChildrenCommonStateUpdate(commonViewState)
+            }
+        }
+    }
+
+    protected open fun handleAggregatedChildrenCommonStateUpdate(commonViewState: CommonViewState) {
+        updateState { state ->
+            state.copyCommon(commonViewState)
+        }
+    }
+
+    private fun attachChildren(args: TArgs) {
+        childViewModels.forEach { childViewModel ->
+            childViewModel.cancelCallbackSubscriptions()
+            childViewModel.subscribeExternalEvents {
+                if (it is CommonExternalEvent) {
+                    handleExternalEvent(it)
+                } else {
+                    handleChildExternalEvent(it)
+                }
+            }
+            @Suppress("UNCHECKED_CAST")
+            childViewModel as MviViewModel<Arguments, *, *, *>
+            childViewModel.attachView(getChildArgumentsInternal(childViewModel, args))
+        }
+    }
+
+    /**
+     * This method should return arguments for given child view model. It is called in [attachView] for every child.
+     *
+     * @param child child view model
+     * @param args arguments passed to [attachView]
+     */
+    protected abstract fun getChildArguments(child: MviViewModel<Arguments, *, *, *>, args: TArgs): Arguments
+
+    private fun getChildArgumentsInternal(child: MviViewModel<Arguments, *, *, *>, args: TArgs): Arguments =
+        _dynamicChildViewModels.find {
+            it.first == child
+        }?.second ?: getChildArguments(child, args)
+
+    override fun onActive() {
+        super.onActive()
+        activateChildren()
+    }
+
+    private fun activateChildren() {
+        childViewModels.forEach {
+            try {
+                it.activate()
+            } catch (ex: IllegalStateException) {
+                val exception = IllegalStateException(
+                    "onActive() called before onViewAttached() on child ViewModel ${this::class.simpleName}"
+                )
+//                viewModelDependencies.logger.log(
+//                    severity = TelemetryLogger.Severity.ERROR,
+//                    message = "onActive() called before onViewAttached() on child ViewModel ${this::class.simpleName}",
+//                    throwable = exception
+//                )
+                throw exception
+            }
+        }
+    }
+
+    override suspend fun handleCommonIntent(intent: CommonIntent) {
+        super.handleCommonIntent(intent)
+
+        when (intent) {
+            CommonIntent.OnBackPressed -> Unit // Do nothing
+            CommonIntent.OnRefresh -> {
+                val refreshableChildren = if (dynamicChildViewModelsRefreshable) {
+                    childViewModels
+                } else {
+                    _initialChildViewModels
+                }
+                refreshableChildren.forEach {
+                    if (it is ModelInternal<*, *, *, *>) {
+                        it.handleCommonIntent(intent)
+                    }
+                }
+            }
+            else -> Unit // Already handled via super
+        }
+    }
+
+    override fun onInactive() {
+//        viewModelDependencies.logger.log(
+//            TelemetryLogger.Severity.DEBUG,
+//            message = "deactivate ${this::class.simpleName} CompositeViewModel"
+//        )
+        deactivateChildren()
+        super.onInactive()
+    }
+
+    private fun deactivateChildren() {
+        childViewModels.forEach { it.deactivate() }
+    }
+
+    override fun onDetached() {
+//        viewModelDependencies.logger.log(
+//            TelemetryLogger.Severity.DEBUG,
+//            message = "detachView ${this::class.simpleName} CompositeViewModel"
+//        )
+        detachChildren()
+        super.onDetached()
+    }
+
+    private fun detachChildren() {
+        childViewModels.forEach { it.detachView() }
+    }
+}
