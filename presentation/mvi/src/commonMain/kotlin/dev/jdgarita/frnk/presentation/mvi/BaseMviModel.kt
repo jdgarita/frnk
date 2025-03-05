a99878888be85dd1626947eb76ea4d6e5cacf4d6
jd <juandiegogarita@gmail.com>
2025-03-04 19:22:50 -0600

    Refactor: Remove unnecessary `CoroutineScope` and `CoroutineDispatcher` imports
    This commit removes unused imports `CoroutineScope` and `CoroutineDispatcher` from `BaseMviModel.kt` as they are not directly used in the code. The commit focuses on cleaning up the imports by removing those are redundant.

--- presentation/mvi/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/mvi/BaseMviModel.kt
+++ presentation/mvi/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/mvi/BaseMviModel.kt
@@ -3,10 +3,7 @@ package dev.jdgarita.frnk.presentation.mvi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.jdgarita.frnk.util.common.Log
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
