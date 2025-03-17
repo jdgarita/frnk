3b36226f72ce9af4a92f7ad27acc7af7dabe6d3b
jd <juandiegogarita@gmail.com>
2025-03-16 21:55:36 -0600

    adds kermit dependency

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -35,6 +35,7 @@ kotlin {
         commonMain.dependencies {
             implementation(project(path = Deps.Main.Frnk.domainConfig))
             implementation(project(path = Deps.Main.Frnk.presentationFramework))
+            implementation(libs.touchlab.kermit)
         }
     }
 }
