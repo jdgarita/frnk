51eb8935f4c78a08211f128a668e4ad3807388ed
jd <juandiegogarita@gmail.com>
2025-03-16 18:29:15 -0600

    Removes JsExport from CommonExternalEvent

--- presentation/mvi/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/mvi/CommonExternalEvent.kt
+++ presentation/mvi/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/mvi/CommonExternalEvent.kt
@@ -1,13 +1,11 @@
 package dev.jdgarita.frnk.presentation.mvi
 
-import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
  * Common [ExternalEvent]s that is handled by all [NavigationRouter]s.
  */
-@JsExport
 sealed class CommonExternalEvent : ExternalEvent {
     @Serializable
     @SerialName("DidPressBack")
