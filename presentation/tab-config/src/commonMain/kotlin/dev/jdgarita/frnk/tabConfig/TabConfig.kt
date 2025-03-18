60de42906b610b00ebecd66d39566ddec6dae9c1
jd <juandiegogarita@gmail.com>
2025-03-17 23:25:57 -0600

    refactors TabConfig to use sealed class

--- presentation/tab-config/src/commonMain/kotlin/dev/jdgarita/frnk/tabConfig/TabConfig.kt
+++ presentation/tab-config/src/commonMain/kotlin/dev/jdgarita/frnk/tabConfig/TabConfig.kt
@@ -6,12 +6,12 @@ import dev.jdgarita.frnk.presentation.resources.images.SemanticIcon
 import dev.jdgarita.frnk.presentation.resources.text.SemanticText
 import dev.jdgarita.frnk.presentation.resources.text.asSemanticText
 
-enum class TabConfig(
+sealed class TabConfig(
     val icon: SemanticIcon,
     val text: SemanticText,
     val navigationContext: NavigationContext
 ) {
-    HOME(
+    object Home: TabConfig(
         icon = SemanticIcon.TabHome,
         // text = Strings.bottom_navigation_item_home.asSemanticText(),
         text = "Home".asSemanticText(),
@@ -20,9 +20,8 @@ enum class TabConfig(
 
     companion object {
         fun getConfigFor(
-            contexts: List<NavigationContext>
-        ): List<TabConfig> = contexts.flatMap { context ->
-            entries.filter { it.navigationContext == context }
-        }
+            contexts: List<NavigationContext>,
+            availableConfigs: List<TabConfig>
+        ): List<TabConfig> = availableConfigs.filter { it.navigationContext in contexts }
     }
 }
