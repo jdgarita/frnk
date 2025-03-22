95abef13896cb79d087d4b26291d4cdc691b40f7
jd <juandiegogarita@gmail.com>
2025-03-22 13:14:38 -0600

    Creates SplashNavigationRouter class and SplashScreen sealed class

---
+++ presentation/identity-presentation/src/commonMain/kotlin/dev/jdgarita/frnk/presentation/identity/navigation/SplashNavigationRouter.kt
@@ -0,0 +1,55 @@
+package dev.jdgarita.frnk.presentation.identity.navigation
+
+import dev.jdgarita.frnk.domain.framework.ScreenName
+import dev.jdgarita.frnk.domain.framework.ScreenNavigationTracker
+import dev.jdgarita.frnk.presentation.framework.navigation.BaseNavigationRouter
+import dev.jdgarita.frnk.presentation.framework.navigation.EmptyNavigationResult
+import dev.jdgarita.frnk.presentation.framework.navigation.NavigationContextSwitcher
+import dev.jdgarita.frnk.presentation.framework.navigation.NavigationRouterResult
+import dev.jdgarita.frnk.presentation.framework.navigation.NavigationScreen
+import dev.jdgarita.frnk.presentation.framework.navigation.NavigationType
+import dev.jdgarita.frnk.presentation.identity.api.AuthNavigationResult
+import dev.jdgarita.frnk.presentation.identity.splash.SplashExternalEvent
+import dev.jdgarita.frnk.presentation.mvi.ExternalEvent
+import kotlinx.coroutines.CoroutineDispatcher
+
+class SplashNavigationRouter(
+    coroutineDispatcher: CoroutineDispatcher,
+    private val navigationContextSwitcher: NavigationContextSwitcher,
+    screenNavigationTracker: ScreenNavigationTracker,
+    private val authorizedFlowStartEvent: ExternalEvent,
+) : BaseNavigationRouter<NavigationRouterResult>(
+    coroutineDispatcher = coroutineDispatcher,
+    navigationContextSwitcher = navigationContextSwitcher,
+    screenNavigationTracker = screenNavigationTracker,
+    navigationResult = AuthNavigationResult.Succeeded
+) {
+
+    override fun processStartEvent(externalEvent: ExternalEvent) {
+        if (externalEvent is SplashStartEvent) {
+            pushScreen(SplashScreen.Splash, NavigationType.FULL_SCREEN)
+        }
+    }
+
+    override fun processExternalEvent(externalEvent: ExternalEvent) {
+        when (externalEvent) {
+            is SplashExternalEvent.DidAuthenticate -> {
+                popAllAndChangeContext(authorizedFlowStartEvent)
+            }
+        }
+    }
+
+    override fun processChildNavigationResult(result: NavigationRouterResult) {}
+
+    private fun popAllAndChangeContext(externalEvent: ExternalEvent) {
+        popAllAndChangeContext(
+            result = EmptyNavigationResult,
+            externalEvent = externalEvent,
+            finishOtherContexts = true
+        )
+    }
+}
+
+sealed class SplashScreen(override val screenName: ScreenName) : NavigationScreen {
+    data object Splash : SplashScreen(ScreenName.Splash)
+}
