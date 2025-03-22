25b6ffdb2b31e93e34d61555b8b8a2c46eb7c1d0
jd <juandiegogarita@gmail.com>
2025-03-22 13:07:48 -0600

    Initial implementation of the identity-presentation-api module
    This commit introduces the `identity-presentation-api` module as a Kotlin Multiplatform library, configuring it for both Android and iOS targets. It includes:
    
    -   **Gradle configuration:** Sets up the module with the Kotlin Multiplatform and Android Library plugins.
    -   **Android target setup:** Configures Android compilation with JVM target 11.
    -   **iOS target setup:** Configures iOS target to create static framework for arm64 and simulator arm64.
    -   **Dependencies:** Adds dependencies for `kotlin-stdlib`, `presentation-mvi`, and `presentation-framework`.
    -   **Android specific settings**: Set namespace, compile and min sdks. Also sets compile options to java 17.
    -  **Git ignore**: Ignores /build folder.

---
+++ presentation/identity-presentation-api/build.gradle.kts
@@ -0,0 +1,55 @@
+import dev.jdgarita.frnk.config.dependencies.Deps
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_11)
+                }
+            }
+        }
+    }
+
+    val xcf = XCFramework()
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "identity-presentation-api"
+            xcf.add(this)
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(project(Deps.Main.Frnk.presentationMvi))
+                implementation(project(Deps.Main.Frnk.presentationFramework))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "dev.jdgarita.frnk.presentation.identity-api"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
