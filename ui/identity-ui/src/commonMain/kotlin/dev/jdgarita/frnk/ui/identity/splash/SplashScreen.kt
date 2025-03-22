d72842a58e7b0f17873efe6dd941c8c158b098de
jd <juandiegogarita@gmail.com>
2025-03-22 13:27:28 -0600

    wip - splash screen

--- ui/identity-ui/src/commonMain/kotlin/dev/jdgarita/frnk/ui/identity/splash/SplashScreen.kt
+++ ui/identity-ui/src/commonMain/kotlin/dev/jdgarita/frnk/ui/identity/splash/SplashScreen.kt
@@ -2,6 +2,7 @@ package dev.jdgarita.frnk.ui.identity.splash
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -15,6 +16,7 @@ fun SplashScreen(
 ) {
     val viewState = viewModel.viewStateFlow.collectAsState(SplashViewState()).value
     Box(modifier = Modifier.fillMaxSize()) {
+        Text("Splash Screen")
         // todo define logo here.
     }
 }
