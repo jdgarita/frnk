d5e7fff60a25d1e9656ec39b61aa240fbad742ed
jd <juandiegogarita@gmail.com>
2025-03-24 21:32:22 -0600

    refactor: add NavigationContextConfig to Koin module provider for improved navigation context management

--- ui/framework-ui/src/commonMain/kotlin/dev/garita/frnk/ui/framework/koin/CommonFrameworkUIKoinModuleProvider.kt
+++ ui/framework-ui/src/commonMain/kotlin/dev/garita/frnk/ui/framework/koin/CommonFrameworkUIKoinModuleProvider.kt
@@ -1,6 +1,8 @@
 package dev.garita.frnk.ui.framework.koin
 
+import dev.garita.frnk.ui.framework.navigation.NavigationContextConfig
 import dev.jdgarita.frnk.util.di.KoinModuleProvider
+import dev.jdgarita.frnk.util.di.set
 import org.koin.core.module.Module
 import org.koin.core.qualifier.StringQualifier
 import org.koin.dsl.module
@@ -10,6 +12,7 @@ class CommonFrameworkUIKoinModuleProvider : KoinModuleProvider {
     override val modules: List<Module>
         get() = listOf(
             module {
+                set<NavigationContextConfig>(navigationContextConfigsQualifier)
             }
         )
 }
